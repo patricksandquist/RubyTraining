9de19d602a9784f46a28f68a66e58e0e679e3918
patricksandquist <patricksandquist@gmail.com>
2015-11-18 17:32:35 -0800

    Solve slow function

--- matrix_region_sum.rb
+++ matrix_region_sum.rb
@@ -4,7 +4,16 @@
 # the same matrix.
 
 def region_sum(matrix, region)
-  
+  top_left = region[0]
+  bottom_right = region[1]
+  sum = 0
+  (top_left[0]..bottom_right[0]).each do |i|
+    (top_left[1]..bottom_right[1]).each do |j|
+      sum += matrix[i][j]
+    end
+  end
+
+  sum
 end
 
 matrix = [[1, 2, 3, 4, 5],
@@ -12,4 +21,4 @@ matrix = [[1, 2, 3, 4, 5],
           [3, 4, 5, 6, 7],
           [4, 5, 6, 7, 8]]
 
-p region_sum(matrix, [[1, 2], [2, 4]])
+puts region_sum(matrix, [[1, 2], [2, 4]]) == 33
