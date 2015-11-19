6f4f3b1d034bd7fe3dcf78d012cc1a08419bf593
patricksandquist <patricksandquist@gmail.com>
2015-11-18 17:27:27 -0800

    Add matrix region sum

---
+++ matrix_region_sum.rb
@@ -0,0 +1,15 @@
+# Given a matrix of integers and coordinates of a rectangular region within
+# the matrix, find the sum of numbers falling inside the rectangle. Our
+# program will be called multiple times with different rectangular regions from
+# the same matrix.
+
+def region_sum(matrix, region)
+  
+end
+
+matrix = [[1, 2, 3, 4, 5],
+          [2, 3, 4, 5, 6],
+          [3, 4, 5, 6, 7],
+          [4, 5, 6, 7, 8]]
+
+p region_sum(matrix, [[1, 2], [2, 4]])
