cba34e8c2adfbac3c4cc17b7e929317c82264dab
patricksandquist <patricksandquist@gmail.com>
2015-12-03 15:38:52 -0500

    Add lcs solution

--- largest_contiguous_sum.rb
+++ largest_contiguous_sum.rb
@@ -1,6 +1,15 @@
 # Given an array of integers (positive and negative) find the largest continuous sum.
 def lcs(arr)
-  
+  max = arr.first || 0 # return zero for empty array
+  current_sum = 0
+
+  arr.each do |num|
+    current_sum += num
+    max = current_sum if current_sum > max
+    current_sum = 0 if current_sum < 0
+  end
+
+  max
 end
 
 puts lcs([5, 3, -7, 6]) == 8
