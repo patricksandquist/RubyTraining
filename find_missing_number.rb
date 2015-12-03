4bf2862f4b3c584530f0c1eeb223c440b51bd336
patricksandquist <patricksandquist@gmail.com>
2015-12-03 16:13:47 -0500

    Add find missing number

---
+++ find_missing_number.rb
@@ -0,0 +1,10 @@
+# There is an array of non-negative integers.
+# A second array is formed by shuffling the elements of the first array and deleting a random element.
+# Given these two arrays, find which element is missing in the second array.
+
+def missing_number(arr1, arr2)
+end
+
+puts missing_number([4, 4, 2, 6, 0, 3], [6, 4, 2, 3, 4]) == 0
+puts missing_number([4, 4, 2, 6, 0, 3], [6, 4, 2, 3, 0]) == 4
+puts missing_number([4], []) == 4
