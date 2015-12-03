a5298d06da31bf92cb30a3fd1c3b1b75c0dc3197
patricksandquist <patricksandquist@gmail.com>
2015-12-03 15:32:00 -0500

    Add largest contiguous sum

---
+++ largest_contiguous_sum.rb
@@ -0,0 +1,9 @@
+# Given an array of integers (positive and negative) find the largest continuous sum.
+def lcs(arr)
+  
+end
+
+puts lcs([5, 3, -7, 6]) == 8
+puts lcs([5, 3, -7, 6, 4]) == 11
+puts lcs([5]) == 5
+puts lcs([]) == 0
