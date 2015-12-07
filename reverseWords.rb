e79dc8d786608c37c74ce07cf15a772fe05db1b8
patricksandquist <patricksandquist@gmail.com>
2015-12-07 11:56:05 -0800

    Add reverse words problem

---
+++ reverseWords.rb
@@ -0,0 +1,13 @@
+# Write a program which reverses the words in an input sentence.
+# The first argument is a file that contains multiple sentences, one per line.
+# Empty lines are also possible. Print to stdout each sentence with the reversed
+# words in it, one per line. Empty lines in the input should be ignored.
+# Ensure that there are no trailing empty spaces in each line you print.
+
+def reverseWords(line)
+  words = line.split(" ")
+  words.reverse.join(" ")
+end
+
+puts reverseWords("Hello world") == "world Hello"
+puts reverseWords("") == ""
