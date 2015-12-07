9474608807748def907f031a4460fc96c625a64c
patricksandquist <patricksandquist@gmail.com>
2015-12-07 11:25:36 -0800

    Add multiplication table problem

---
+++ multiplicationTable.rb
@@ -0,0 +1,14 @@
+# Print out the table in a matrix like fashion, each number formatted to a width
+# of 4 (The numbers are right-aligned and strip out leading/trailing spaces on each line).
+
+(1..12).each do |i|
+  (1..12).each do |j|
+    product = i * j
+    if j == 1
+      print product
+    else
+      print " " * (4 - product.to_s.length) + product.to_s
+    end
+  end
+  puts
+end
