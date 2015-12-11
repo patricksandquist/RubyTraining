91d8caeb420064845a41bb75614e11199eaa5bc6
patricksandquist <patricksandquist@gmail.com>
2015-12-11 12:33:29 -0800

    Add reverser problem

---
+++ string_reverser.rb
@@ -0,0 +1,10 @@
+# Given an input string, reverse all the words.
+def reverser(str)
+end
+
+# Time complexity is O(n).
+
+puts reverser("Hello world!") === "world! Hello"
+puts reverser("Hello    world!") === "world! Hello"
+puts reverser("My name is Patrick!") === "Patrick! is name My"
+puts reverser("") === ""
